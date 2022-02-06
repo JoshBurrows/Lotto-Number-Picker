550c0a293f06c27a8349e3eaf920ae786fb8f3ac
Josh Burrows <jburrowsDev@gmail.com>
2022-02-05 20:36:44 -0600

    working on styles

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-// generate 5 random numbers 1 - 69 inclusive
-// generate 1 random number 1 - 26 inclusive
-// place random numbers in order from smallest to largest
-// store random numbers in variable
-// use random numbers 1 - 69 on balls 1 - 5
-// use random numbers 1 - 26 on pball
 const balls = document.getElementsByClassName("ball");
 const randomBall = (maxNum) => Math.floor(Math.random() * maxNum) + 1;
 let numbers = [];
+/* Generate 5 random numbers 1-69 inclusive on button click */
 const clicked = () => {
   for (let i = 0; i < 5; i++) {
     numbers[i] = randomBall(69);
   }
+  /*uses numbers array to create a new array without duplicate numbers if any. */
   let displayNumbers = [...new Set(numbers)];
   if (displayNumbers.length != numbers.length) {
-    displayNumbers.push(randomBall(69));
-    // console.log(
-    //   `${displayNumbers[displayNumbers.length - 1]} pushed to newNumbers`
-    // );
+    displayNumbers.splice(0, 0, randomBall(69));
+    /* console.log(
+       `${displayNumbers[displayNumbers.length - 1]} pushed to newNumbers`
+     ); */
   }
 
   displayNumbers.sort((a, b) => a - b);
